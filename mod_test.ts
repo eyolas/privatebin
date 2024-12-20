3e82d3a875288ff3c63d7e39ba74f01795877b3d
eyolas <dtouzet@gmail.com>
2024-12-20 08:08:16 +0100

    chore: update dependencies and add tests for PrivatebinClient functionality

---
+++ mod_test.ts
@@ -0,0 +1,55 @@
+import { assertEquals, assertRejects } from "@std/assert";
+import { createKey, PrivatebinClient, type PrivatebinOptions } from "./mod.ts";
+
+Deno.test('Create a privatebin paste and read', async () => {
+    const privatebin = new PrivatebinClient();
+    const key = createKey();
+    const msg = "Hello World!";
+    const opts = {
+        textformat: "plaintext",
+        expire: "1week",
+        burnafterreading: 1,
+        opendiscussion: 0,
+        output: "text",
+        compression: "zlib",
+    } satisfies PrivatebinOptions;
+    const paste = await privatebin.sendText(msg, key, opts);
+    const decrypted = await privatebin.getText(paste.id, key);
+    assertEquals(decrypted.paste, msg);
+});
+
+Deno.test('Create a privatebin paste and read with different key and throw error', async () => {
+    const privatebin = new PrivatebinClient();
+    const key = createKey();
+    const key2 = createKey();
+    const msg = "Hello World!";
+    const opts = {
+        textformat: "plaintext",
+        expire: "1week",
+        burnafterreading: 1,
+        opendiscussion: 0,
+        output: "text",
+        compression: "zlib",
+    } satisfies PrivatebinOptions;
+    const paste = await privatebin.sendText(msg, key, opts);
+    await assertRejects(() => privatebin.getText(paste.id, key2));
+});
+
+
+Deno.test('Read a burn privatebin', async () => {
+    const privatebin = new PrivatebinClient();
+    const key = createKey();
+    const msg = "Hello World!";
+    const opts = {
+        textformat: "plaintext",
+        expire: "1week",
+        burnafterreading: 1,
+        opendiscussion: 0,
+        output: "text",
+        compression: "zlib",
+    } satisfies PrivatebinOptions;
+    const paste = await privatebin.sendText(msg, key, opts);
+    const decrypted = await privatebin.getText(paste.id, key);
+    assertEquals(decrypted.paste, msg);
+    await assertRejects(() => privatebin.getText(paste.id, key));
+});
